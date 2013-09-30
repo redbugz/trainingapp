d13b7922900c40823dc17dc31e31b3fe0a9ff0f5
Logan Allred <allredlt@familysearch.org>
2013-09-30 14:12:36 -0600

    Try creating a jenkins proxy to workaround connection issues.

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ var woodruff = require("woodruff")
  * Expose the app
  */
 var app = module.exports = woodruff(__dirname, shared);
+
+
+/**
+ * Dev Proxies
+ */
+//app.configure('development', function() {
+  var proxy = require("simple-http-proxy");
+
+  app.stack.unshift({
+    route: "/jenkins/",
+    handle: proxy("http://54.221.235.240:8080/")
+  });
+
+//});
